490f76718e384ff7e782fdc13fc919d406a241a0
adithyanvs <adithyanvs@gmail.com>
2022-07-07 06:50:36 +0530

    first committ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LoginComponent } from './Components/login/login.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { EventformComponent } from './Components/eventform/eventform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    EventformComponent
   ],
   imports: [
     BrowserModule,
